6e92aa510e0e2059e9fb182c5c492ee440c0b584
Alan S. Martins <50818058+alanosms@users.noreply.github.com>
2022-05-24 14:42:23 -0300

    Day 2: Operators

---
+++ daytwo.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class daytwo {
+    public static void main(String args[]) {
+        double meal_cost;
+        int tip_percent, tax_percent;
+        Scanner scan = new Scanner(System.in);
+        
+        meal_cost = scan.nextDouble();
+        tip_percent = scan.nextInt();
+        tax_percent = scan.nextInt();
+        
+        double tip = meal_cost * tip_percent / 100;
+        double tax = meal_cost * tax_percent / 100;
+
+        double total = meal_cost + tip + tax;
+        int totalCost = (int) Math.round(total);
+        System.out.println(totalCost);
+    }
+}
+
