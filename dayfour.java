da5e6d9ec87a2fa36e9d377ec350ca3db8087fdb
Alan S. Martins <50818058+alanosms@users.noreply.github.com>
2022-05-25 10:52:05 -0300

    Day 3: Intro to Conditional Statements

---
+++ dayfour.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class dayfour {
+    public static void main(String args[]) {
+        int input;
+        Scanner scan = new Scanner(System.in);
+
+        input = scan.nextInt();
+        if (input < 1 && input > 100) //* INPUT RULE 
+            return;
+        if (input % 2 != 0) {
+            System.out.println("Weird");
+        } 
+        else if (input > 2 && input < 5) {
+            System.out.println("Not Weird");
+        }
+        else if (input > 6 && input <= 20) {
+            System.out.println("Weird");
+        }
+        else if (input > 20) {
+            System.out.println("Not Weird");
+        }
+    }
+}
