633ab51497c3c2ecd3ae103f63b4f39a063fba0a
Alan S. Martins <50818058+alanosms@users.noreply.github.com>
2022-05-29 11:34:56 -0300

    Day 7: Arrays

---
+++ dayseven.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class dayseven {
+    public static void main(String args[]){
+        Scanner scan = new Scanner(System.in);
+        int numberOfCases = scan.nextInt();
+        int[] myArray;
+        myArray = new int[numberOfCases]; 
+        for(int initialCount = 0; initialCount < numberOfCases; initialCount++){
+            myArray[initialCount] = scan.nextInt();
+
+        }
+        int length = myArray.length - 1;
+        for(int initialCount = length; initialCount > -1; initialCount--){
+            System.out.print(myArray[initialCount]);
+            System.out.print(" ");
+        }
+}
+}
